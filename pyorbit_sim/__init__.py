795c8eb4ff6fd30fe9e40da9a9b4ac0596735abf
Hoover, Austin <hooveram@ornl.gov>
2023-01-27 11:52:24 -0500

    Try updating __init__ files

---
+++ pyorbit_sim/__init__.py
@@ -0,0 +1,6 @@
+import pyorbit_sim.bunch_utils
+import pyorbit_sim.data
+import pyorbit_sim.lattice
+import pyorbit_sim.misc
+import pyorbit_sim.plotting
+import pyorbit_sim.utils
---
+++ pyorbit_sim/btf/__init__.py
@@ -0,0 +1,2 @@
+import pyorbit_sim.btf.lattice
+import pyorbit_sim.btf.sim
