2f5ea929c42b0f1ee9e926b00123b491ba69c0d3
Hoover, Austin <hooveram@ornl.gov>
2023-05-18 17:11:56 -0400

    2023-05-18 SNS_LINAC PARMTEQ bunch to WS07
    8.44e6 particles, transporting to "measurement plane" WS07 in MEBT.
    42 mA.

--- scripts/SNS_LINAC/sim.py
+++ scripts/SNS_LINAC/sim.py
@@ -18,6 +18,7 @@ from bunch_utils_functions import copyCoordsToInitCoordsAttr
 from linac import BaseRfGap
 from linac import BaseRfGap_slow
 from linac import MatrixRfGap
+from linac import RfGapThreePointTTF
 from linac import RfGapThreePointTTF_slow
 from linac import RfGapTTF
 from linac import RfGapTTF_slow
@@ -35,6 +36,7 @@ from orbit.py_linac.lattice import LinacPhaseApertureNode
 from orbit.py_linac.lattice import AxisFieldRF_Gap
 from orbit.py_linac.lattice import AxisField_and_Quad_RF_Gap
 from orbit.py_linac.lattice import BaseRF_Gap
+from orbit.py_linac.lattice import OverlappingQuadsNode 
 from orbit.py_linac.lattice_modifications import Add_quad_apertures_to_lattice
 from orbit.py_linac.lattice_modifications import Add_rfgap_apertures_to_lattice
 from orbit.py_linac.lattice_modifications import AddMEBTChopperPlatesAperturesToSNS_Lattice
@@ -63,7 +65,7 @@ from pyorbit_sim.utils import ScriptManager
 # Setup
 # --------------------------------------------------------------------------------------
 
-save = True  # no output if False
+save = False  # no output if False
 
 # MPI
 _mpi_comm = orbit_mpi.mpi_comm.MPI_COMM_WORLD
@@ -117,7 +119,7 @@ if _mpi_rank == 0:
     print("lattice length = {:.3f} [m])".format(lattice.getLength()))
 
 
-# Save lattice structure.
+# Save lattice structure to file.
 if save and _mpi_rank == 0:
     # Node start/stop positions.
     file = open(man.get_filename("lattice_nodes.txt"), "w")
@@ -134,12 +136,8 @@ if save and _mpi_rank == 0:
 
     
 # Set the RF gap model.
-#   BaseRfGap / BaseRfGap_slow: uses only E0TL * cos(phi) * J0(kr), with E0TL = const.
-#   MatrixRfGap / MatrixRfGap_slow: uses a matrix approach like envelope codes.
-#   RfGapTTF / RfGapTTF_slow: uses Transit Time Factors (TTF) like PARMILA.
-rf_gap_model = RfGapTTF
 for rf_gap in lattice.getRF_Gaps():
-    rf_gap.setCppGapModel(rf_gap_model())
+    rf_gap.setCppGapModel(RfGapTTF())
 
     
 # Set overlapping RF and quad fields.
@@ -149,35 +147,51 @@ z_step = 0.002
 ## Only RF gaps will be replaced with non-zero length models. Quads stay hard-edged. 
 ## Such approach will not work for DTL cavities - RF and quad fields are overlapped 
 ## for DTL.
-# seq_names = ["MEBT", "CCL1", "CCL2", "CCL3", "CCL4", "SCLMed"]
-# Replace_BaseRF_Gap_to_AxisField_Nodes(lattice, z_step, fields_filename, seq_names)
+if False:
+    seq_names = ["MEBT", "CCL1", "CCL2", "CCL3", "CCL4", "SCLMed"]
+    Replace_BaseRF_Gap_to_AxisField_Nodes(lattice, z_step, fields_filename, seq_names)
 
 ## Hard-edge quad models will be replaced with soft-edge models. It is possible for DTL 
 ## also - if the RF gap models are zero-length ones. 
-#
-# seq_names = ["MEBT", "DTL1", "DTL2", "DTL3", "DTL4", "DTL5", "DTL6"]
-# Replace_Quads_to_OverlappingQuads_Nodes(lattice, z_step, seq_names, [], SNS_EngeFunctionFactory)
+if False:
+    seq_names = ["MEBT", "DTL1", "DTL2", "DTL3", "DTL4", "DTL5", "DTL6"]
+    Replace_Quads_to_OverlappingQuads_Nodes(lattice, z_step, seq_names, [], SNS_EngeFunctionFactory)
 
 ## Hard-edge quad and zero-length RF gap models will be replaced with soft-edge quads
 ## and field-on-axis RF gap models. Can be used for any sequences, no limitations.
-#
-# seq_names = ["MEBT", "DTL1", "DTL2", "DTL3", "DTL4", "DTL5", "DTL6"]
-# Replace_BaseRF_Gap_and_Quads_to_Overlapping_Nodes(
-#     lattice, z_step, fields_filename, seq_names, [], SNS_EngeFunctionFactory
-# )
+if False:
+    seq_names = ["MEBT", "DTL1", "DTL2", "DTL3", "DTL4", "DTL5", "DTL6"]
+    Replace_BaseRF_Gap_and_Quads_to_Overlapping_Nodes(
+        lattice, z_step, fields_filename, seq_names, [], SNS_EngeFunctionFactory
+    )
+
+
+# Set longitudinal fields
+if False:
+    # Use linac-style quads and drifts instead of TEAPOT style. (Useful when 
+    # the energy spread is large, but is slower and is not symplectic.)
+    lattice.setLinacTracker(True)
+
+    # Add tracking through the longitudinal field component of the quad. The
+    # longitudinal component is nonzero only for the distributed magnetic field
+    # of the quad. This means we have to modify the lattice with 
+    # `Replace_BaseRF_Gap_and_Quads_to_Overlapping_Nodes` or 
+    # `Replace_Quads_to_OverlappingQuads_Nodes`.
+    for node in lattice.getNodes():
+        if (isinstance(node, OverlappingQuadsNode) or isinstance(node, AxisField_and_Quad_RF_Gap)):
+            node.setUseLongitudinalFieldOfQuad(True)
 
 
 # Add space charge nodes.
 sc_solver = "3D"  # {"3D", "ellipsoid", None}
-sc_grid_size = (64, 64, 64)
+sc_grid_size = (128, 128, 128)
 sc_path_length_min = 0.01
 sc_n_bunches = 1
-sc_freq = 402.5e6
 if sc_solver == "3D":
     sc_calc = SpaceChargeCalc3D(sc_grid_size[0], sc_grid_size[1], sc_grid_size[2])
     if sc_n_bunches > 1: 
         sc_calc.numExtBunches(n_bunches)
-        sc_calc.freqOfBunches(freq)
+        sc_calc.freqOfBunches(rf_frequency)
     sc_nodes = setSC3DAccNodes(lattice, sc_path_length_min, sc_calc)
 elif sc_solver == "ellipsoid":
     n_ellipsoids = 1
@@ -193,8 +207,7 @@ if sc_solver is not None and _mpi_rank == 0:
 
 
 # Add transverse aperture nodes.
-add_transverse_apertures = True
-if add_transverse_apertures:
+if True:
     x_size = 0.042  # [m]
     y_size = 0.042  # [m]
     aperture_nodes = Add_quad_apertures_to_lattice(lattice)
@@ -206,24 +219,21 @@ if add_transverse_apertures:
     if _mpi_rank == 0:
         print("Added {} transverse aperture nodes.".format(len(aperture_nodes)))
 
-# Add longitudinal (phase, energy) aperture nodes after every RF gap node.
-add_energy_apertures = True
-add_phase_apertures = True
+# Add longitudinal (phase, energy) aperture nodes each RF gap node.
 node_pos_dict = lattice.getNodePositionsDict()
-rf_gap_nodes = lattice.getNodesOfClasses([BaseRF_Gap, AxisFieldRF_Gap, AxisField_and_Quad_RF_Gap])
-for node in rf_gap_nodes[::2]:
+for node in lattice.getNodesOfClasses([BaseRF_Gap, AxisFieldRF_Gap, AxisField_and_Quad_RF_Gap]):
     if node.hasParam("aperture") and node.hasParam("aprt_type"):
         position_start, position_stop = node_pos_dict[node]
-        if add_phase_apertures:
+        if True:
             aperture_node = LinacPhaseApertureNode(frequency=rf_frequency, name="{}_phase_aprt_out".format(node.getName()))
             aperture_node.setMinMaxPhase(-180.0, 180.0)  # [deg]
             aperture_node.setPosition(position_stop)
             aperture_node.setSequence(node.getSequence())
             node.addChildNode(aperture_node, node.EXIT)
             aperture_nodes.append(aperture_node)
-        if add_energy_apertures:
+        if True:
             aperture_node = LinacEnergyApertureNode(name="{}_energy_aprt_out".format(node.getName()))
-            aperture_node.setMinMaxEnergy(-0.010, +0.010)  # [GeV]
+            aperture_node.setMinMaxEnergy(-0.100, +0.100)  # [GeV]
             aperture_node.setPosition(position_stop)
             aperture_node.setSequence(node.getSequence())
             node.addChildNode(aperture_node, node.EXIT)
@@ -231,10 +241,6 @@ for node in rf_gap_nodes[::2]:
 if _mpi_rank == 0:
     print("Added {} longitudinal aperture nodes.".format(len(aperture_nodes) - n_transverse_aperture_nodes))
 
-    
-# Use linac-style quads and drifts instead of TEAPOT style. (Useful when 
-# the energy spread is large, but is slower and is not symplectic.)
-lattice.setLinacTracker(False)
 
 
 # Bunch
@@ -245,14 +251,14 @@ bunch = Bunch()
 bunch.mass(0.939294)  # [GeV / c^2]
 bunch.charge(-1.0)  # [elementary charge units]
 bunch.getSyncParticle().kinEnergy(0.0025)  # [GeV]
-current = 0.042  # [C/s]
+current = 0.042  # [A]
 intensity = (current / rf_frequency) / abs(float(bunch.charge()) * consts.charge_electron)
 gamma = bunch.getSyncParticle().gamma()
 beta = bunch.getSyncParticle().beta()
 
 # Load the bunch coordinates.
 # bunch_filename = None
-bunch_filename = "/home/46h/projects/BTF/sim/data/RFQ_output_PARMTEQ_50mA_42mA_1.00e+06_decorr_x-y-z.dat"
+bunch_filename = "/home/46h/projects/BTF/sim/data/RFQ_output_PARMTEQ_50mA_42mA_8.66e+06.dat"
 if bunch_filename is None:
     if _mpi_rank == 0:
         print("Generating bunch from Twiss parameters.")    
@@ -281,6 +287,13 @@ else:
     if _mpi_rank == 0:
         print("Generating bunch from file '{}'.".format(bunch_filename))
     bunch.readBunch(bunch_filename)
+    
+# Shift the longitudinal bunch position.
+phase_offset = 0.0  # [deg]
+if math.abs(phase_offset) > 1.0e-30:
+    z_to_phase_coeff = pyorbit_sim.bunch_utils.get_z_to_phase_coeff(bunch, rf_frequency)
+    z_offset = -phase_offset / z_to_phase_coeff
+    pyorbit_sim.bunch_utils.shift(bunch, delta=[0.0, 0.0, 0.0, 0.0, z_offset, 0.0], verbose=True)
         
 # Downsample by random selection. 
 ## Here we assume the particles were randomly generated to begin with, so we
@@ -292,7 +305,6 @@ samples = None
 if samples is not None and bunch_size_global > samples:
     new_bunch = Bunch()
     bunch.copyEmptyBunchTo(new_bunch)
-    print(new_bunch.getSize())
     for i in range(samples):
         new_bunch.addParticle(
             bunch.x(i),
@@ -307,15 +319,13 @@ if samples is not None and bunch_size_global > samples:
     bunch_size_global = bunch.getSizeGlobal()
 
 # Decorrelate x-y-z.
-decorrelate_x_y_z = False
-if decorrelate_x_y_z:
+if False:
     bunch = pyorbit_sim.bunch_utils.decorrelate_x_y_z(bunch, verbose=True)
 
-# If `dist` is not None, generate an RMS-equivalent distribution in x-x', y-y', and z-z' 
-# using an analytic distribution function (Gaussian, KV, Waterbag). Reconstruct the the 
-# six-dimensional distribution as f(x, x', y, y', z, z') = f(x, x') f(y, y') f(z, z').
-dist = None
-if dist is not None:
+# Generate an RMS-equivalent distribution in x-x', y-y', and z-z' using an analytic 
+# distribution function (Gaussian, KV, Waterbag). Reconstruct the the six-dimensional 
+# distribution as f(x, x', y, y', z, z') = f(x, x') f(y, y') f(z, z').
+if False:
     n_parts = bunch_size_global
     if _mpi_rank == 0:
         print("Repopulating bunch using 2D Twiss parameters and {} generator.".format(dist))
@@ -333,12 +343,16 @@ if dist is not None:
     alpha_x = bunch_twiss_analysis.getEffectiveAlpha(0)
     alpha_y = bunch_twiss_analysis.getEffectiveAlpha(1)
     alpha_z = bunch_twiss_analysis.getEffectiveAlpha(2)    
-    dist = dist(
-        twissX=TwissContainer(alpha_x, beta_x, eps_x),
-        twissY=TwissContainer(alpha_y, beta_y, eps_y),
-        twissZ=TwissContainer(alpha_z, beta_z, eps_z),
+    bunch = pyorbit_sim.bunch_utils.generate(
+        dist=WaterBagDist3D(
+            twissX=TwissContainer(alpha_x, beta_x, eps_x),
+            twissY=TwissContainer(alpha_y, beta_y, eps_y),
+            twissZ=TwissContainer(alpha_z, beta_z, eps_z),
+        ),
+        n_parts=n_parts, 
+        bunch=bunch, 
+        verbose=True,
     )
-    bunch = pyorbit_sim.bunch_utils.generate(dist=dist, n_parts=n_parts, bunch=bunch, verbose=True)
     
 # Set the macrosize.
 bunch_size_global = bunch.getSizeGlobal()
@@ -373,23 +387,21 @@ if _mpi_rank == 0:
 # copyCoordsToInitCoordsAttr(bunch)
 
 
+
 # Tracking
 # --------------------------------------------------------------------------------------
 
 start = 0  # start node (name or position)
-stop = 30.0  # stop node (name or position)s
-save_input_bunch = True
-save_output_bunch = True
+stop = "MEBT_Diag:WS07"  # stop node (name or position)
 
-
-# Create bunch writer/plotter/monitor nodes.
+# Create bunch writer.
 writer = pyorbit_sim.linac.BunchWriter(
     folder=man.outdir, 
     prefix=man.prefix, 
-    index=1, 
+    index=0, 
 )
     
-    
+# Create bunch plotter.
 def transform(X):
     """Normalize the 2D phase spaces."""
     Sigma = np.cov(X.T)
@@ -416,7 +428,7 @@ plotter.add_function(
     bins=32,
 )
 
-
+# Create bunch monitor.
 monitor = pyorbit_sim.linac.Monitor(
     position_offset=0.0,  # will be set automatically in `pyorbit_sim.linac.track`.
     stride={
@@ -430,9 +442,9 @@ monitor = pyorbit_sim.linac.Monitor(
     track_rms=True,
     dispersion_flag=False,
     emit_norm_flag=False,
+    rf_frequency=rf_frequency,
     verbose=True,
 )
-
 
 # Record synchronous particle time of arrival at each accelerating cavity.
 if _mpi_rank == 0:
@@ -442,19 +454,13 @@ if _mpi_rank == 0:
     print("Design bunch tracking complete.")
     
 # Save input bunch.
-if save and save_input_bunch:
-    if start is None or type(start) is not str:
-        filename = man.get_filename("bunch_0_START.dat")
-    else:
-        filename = man.get_filename("bunch_0_{}.dat".format(start))
-    if _mpi_rank == 0:
-        print("Saving bunch to file {}".format(filename))
-    bunch.dumpBunch(filename)    
+if save and True:
+    node_name = "START" if (start is None or type(start) is not str) else start
+    writer.action(bunch, node_name)
     
 # Track
 if _mpi_rank == 0:
     print("Tracking...")
-        
 params_dict = pyorbit_sim.linac.track(
     bunch, 
     lattice, 
@@ -464,27 +470,16 @@ params_dict = pyorbit_sim.linac.track(
     verbose=True
 )
 
-# Save history array.
+# Save scalar history.
 if _mpi_rank == 0 and monitor.track_history and save:
     filename = man.get_filename("history.dat")
     print("Writing history to {}".format(filename))
     monitor.write_history(filename, delimiter=",")
     
-    
-# Save lost particles.
-lostbunch = params_dict["lostbunch"]
-aprt_nodes_losses = GetLostDistributionArr(aperture_nodes, lostbunch)
-total_loss = 0.0
-for (node, loss) in aprt_nodes_losses:
-    if _mpi_rank == 0:
-        print(
-            "node={:30s},".format(node.getName()), 
-            "pos={:9.3f},".format(node.getPosition()), 
-            "loss= {:6.0f}".format(loss),
-        )
-    total_loss += loss
+# Save losses.
+aprt_nodes_losses = GetLostDistributionArr(aperture_nodes, params_dict["lostbunch"])
 if _mpi_rank == 0:
-    print("Total loss = {:.2e}".format(total_loss))
+    print("Total loss = {:.2e}".format(sum([loss for (node, loss) in aprt_nodes_losses])))
 if save:
     filename = man.get_filename("losses.txt")
     if _mpi_rank == 0:
@@ -495,16 +490,10 @@ if save:
         file.write("{} {} {}\n".format(node.getName(), node.getPosition(), loss))
     file.close()
     
-    
 # Save output bunch.
-if save and save_output_bunch:
-    if stop is None or type(stop) is not str:
-        filename = man.get_filename("bunch_{}_STOP.dat".format(writer.index))
-    else:
-        filename = man.get_filename("bunch_{}_{}.dat".format(writer.index, stop))
-    if _mpi_rank == 0:
-        print("Saving bunch to file {}".format(filename))
-    bunch.dumpBunch(filename)
+if save and True:
+    node_name = "STOP" if (stop is None or type(stop) is not str) else stop
+    writer.action(bunch, node_name)
 
 if _mpi_rank == 0:
     print("timestamp = {}".format(man.timestamp))
