e06302c0c04a30811288c0b7985867ab18459838
Hoover, Austin <hooveram@ornl.gov>
2023-08-03 16:04:36 -0400

    Improve ScriptManager

--- pyorbit_sim/utils.py
+++ pyorbit_sim/utils.py
@@ -76,7 +76,27 @@ def url_style(url, text=None):
 
 class ScriptManager:
     
-    def __init__(self, datadir=None, path=None, timestamp=None, datestamp=None):
+    def __init__(self, datadir=None, path=None, timestamp=None, datestamp=None, script_path_in_outdir=True):
+        """
+        Parameters
+        ----------
+        datadir : str
+            All output files/folders will go here.
+        path : pathlib.Path
+            The path to the script.
+        timestamp : str
+            Timestamp when ScriptManager was created (YYMMDDHHSSMM). This is
+            appended to each output file, along with the script name. For example,
+            "230812062403-sim_coords.py" if the script name is 'sim'.
+        datestamp : str
+            Datestamp when ScriptManager was created (YYYY-MM-DD).
+        script_path_in_outdir : bool
+            Suppose the script is '/scripts/ring/sim.py` and datadir is
+            '/home/sim_data/' and timestamp is '230812062403':
+            
+            True: outdir is '/home/sim_data/ring/sim/230812062403/'.
+            False: outdir is '/home/sim_data/'.
+        """
         self.datadir = datadir
         self.timestamp = timestamp
         self.datestamp = datestamp
@@ -84,17 +104,23 @@ class ScriptManager:
         self.script_name = self.path.stem
         self.git_hash, self.git_url = self.get_git()
         self.prefix = "{}-{}".format(self.timestamp, self.script_name)
-        self.outdir = os.path.join(
-            self.datadir, 
-            self.path.as_posix().split("scripts/")[1].split(".py")[0], 
-            self.datestamp,
-        )
-        
+        self.script_path_in_outdir = script_path_in_outdir
+        if self.script_path_in_outdir:
+            self.outdir = os.path.join(
+                self.datadir, 
+                self.path.as_posix().split("scripts/")[1].split(".py")[0], 
+                self.datestamp,
+            )
+        else:
+            self.outdir = self.datadir
+    
     def make_outdir(self):
+        """Create output directory."""
         if not os.path.exists(self.outdir):
             os.makedirs(self.outdir)
 
     def get_git(self):
+        """Retreive git information."""
         _mpi_comm = orbit_mpi.mpi_comm.MPI_COMM_WORLD
         _mpi_rank = orbit_mpi.MPI_Comm_rank(_mpi_comm)
 
@@ -109,12 +135,18 @@ class ScriptManager:
         return _git_hash, _git_url
     
     def get_filename(self, filename, sep="_"):
+        """Append prefix to filename.
+        
+        Example: 'data.txt' --> '230812062403-sim_data.txt'.
+        """
         return os.path.join(self.outdir, "{}{}{}".format(self.prefix, sep, filename))
     
     def save_script_copy(self):
+        """Save a timestamped copy of the script"""
         shutil.copy(self.path.absolute().as_posix(), self.get_filename(".py", sep=""))
         
     def get_info(self):
+        """Return info dictionary."""
         info = {
             "git_hash": self.git_hash,
             "git_url": self.git_url,
@@ -125,7 +157,8 @@ class ScriptManager:
         }
         return info
 
-    def save_info(self):      
+    def save_info(self):   
+        """Save info dictionary."""
         info = self.get_info()
         file = open(self.get_filename("info.txt"), "w")
         for key in sorted(info):
